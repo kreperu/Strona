074dfd3c5d3fd5221dfddc9b56d487df56e92433
kreperu <thekreperu@gmail.com>
2024-11-12 18:23:44 +0100

    update

--- dataRequests.js
+++ dataRequests.js
@@ -1,9 +1,6 @@
-/*type weatherData = {
-    data: string;
-}*/
 async function requestData() {
     //TODO use mapapogoda.pl:8000/data/
-    let response = await fetch("http://192.168.1.70:8000/data/", {
+    let response = await fetch("http://83.22.183.251:8000/data/", {
         method: "GET"
     });
     return response.json();
